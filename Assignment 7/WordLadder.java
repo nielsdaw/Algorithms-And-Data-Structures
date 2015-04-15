b1603571f67721c9a4879531453329e56d376138
jonastonny <jonastonnynielsen@gmail.com>
2015-04-15 21:03:43 +0200

    yes yes yes

--- Assignment 7/WordLadder.java
+++ Assignment 7/WordLadder.java
@@ -1,32 +1,14 @@
 import java.util.Arrays;
+import java.lang.Iterable;
+import java.util.Iterator;
 
 public class WordLadder{
 
-	// public static boolean containLetters(String s1, String s2){
-	// 	String tmp1 = sortString(s1.substring(1, s1.length()));
-	// 	String tmp2 = sortString(s2);
-
-	// 	for (int x = 0; x < 5 ; x++ ) {
-	// 		if(x == 0){
-	// 			String tmp3 = tmp2.substring(x, tmp2.length()-1);
-	// 			if(tmp1.equals(tmp3)){return true;}
-	// 		}
-	// 		else if(x == tmp2.length()-1){
-	// 			String tmp3 = tmp2.substring(tmp2.length() - x, tmp2.length());	
-	// 			if(tmp1.equals(tmp3)){return true;}
-	// 		}
-	// 		else{
-	// 			String tmp3 = tmp2.substring(0, x) + tmp2.substring(x+1);
-	// 			if(tmp1.equals(tmp3)){return true;}
-	// 		}
-	// 	}
-	// 	return false;
-	// }
-
-
-	public static void mapFourLetters(String s, ST st){
+	public static void mapFourLetters(String s, ST st, int index){
 		//String tmp1 = sortString(s1.substring(1, s1.length()));
 		String inputString = sortString(s);
+		Vertex v = new Vertex(s, index);
+		boolean seen = false;
 
 		for (int x = 0; x < 5 ; x++ ) {
 			LinkedBag lb = new LinkedBag();
@@ -34,42 +16,60 @@ public class WordLadder{
 				String inputStringShort = inputString.substring(x, inputString.length()-1);
 				if(st.contains(inputStringShort)) {
 					lb = (LinkedBag) st.get(inputStringShort);
-					lb.add(s);
-					st.put(inputStringShort, lb);
-					StdOut.println(inputStringShort + " already exists");
+					for (Object o: lb) {
+						Vertex vert = (Vertex) o;
+						if (vert.getWord().equals(s)) {
+							seen = true;
+						}
+					}
+					if(!seen) {
+						lb.add(v);
+						st.put(inputStringShort, lb);
+					}
 				} 
 				else {
-					lb.add(inputString);
+					lb.add(v);
 					st.put(inputStringShort, lb);
-					StdOut.println("added: " + inputStringShort);
 				}	
 			}
 			else if(x == inputString.length()-1){
 				String inputStringShort = inputString.substring(inputString.length() - x, inputString.length());
 				if(st.contains(inputStringShort)) {
 					lb = (LinkedBag) st.get(inputStringShort);
-					lb.add(s);
-					st.put(inputStringShort, lb);
-					StdOut.println(inputStringShort + " already exists");
-				} 
+					for (Object o: lb) {
+						Vertex vert = (Vertex) o;
+						if (vert.getWord().equals(s)) {
+							seen = true;
+						}
+					}
+					if(!seen) {
+						lb.add(v);
+						st.put(inputStringShort, lb);
+					}
+				}
 				else {
-					lb.add(inputString);
+					lb.add(v);
 					st.put(inputStringShort, lb);
-					StdOut.println("added: " + inputStringShort);
 				}
 			}
 			else{
 				String inputStringShort = inputString.substring(0, x) + inputString.substring(x+1);
 				if(st.contains(inputStringShort)) {
 					lb = (LinkedBag) st.get(inputStringShort);
-					lb.add(s);
-					st.put(inputStringShort, lb);
-					StdOut.println(inputStringShort + " already exists");
-				} 
+					for (Object o: lb) {
+						Vertex vert = (Vertex) o;
+						if (vert.getWord().equals(s)) {
+							seen = true;
+						}
+					}
+					if(!seen) {
+						lb.add(v);
+						st.put(inputStringShort, lb);
+					}
+				}
 				else {
-					lb.add(inputString);
+					lb.add(v);
 					st.put(inputStringShort, lb);
-					StdOut.println("added: " + inputStringShort);
 				}
 			}
 		}
@@ -85,7 +85,7 @@ public class WordLadder{
 		return sorted;
 	}
 
-	public static void shortestPath(String scource, String vertex, Digraph graph, LinearProbingHashST<String, Integer> vertices){
+	public static void shortestPath(String scource, String vertex, Digraph graph, ST<String, Integer> vertices){
 		int scourceIndex = vertices.get(scource);
 		int vertexIndex = vertices.get(vertex);
 		BreadthFirstDirectedPaths p = new BreadthFirstDirectedPaths(graph, scourceIndex);
@@ -99,58 +99,43 @@ public class WordLadder{
 	}
 
 	public static void main(String[] args) {
-		//ST<String, LinkedBag> vertices = new ST<String, LinkedBag>();
 		ST<String, LinkedBag> fourLetterWords = new ST<String, LinkedBag>();
+		ST<String, Integer> indexFix = new ST<String, Integer>();
 		
 		String[] inputWords = StdIn.readAllStrings();
+		Arrays.sort(inputWords);
 
 		String[] inputWordsFour = new String[inputWords.length];
-
-		//mapFourLetters(inputWords[w], fourLetterWords);
 
 		// Put all words in a sorted structure. Values are bags containing the original words
 		// from which the four-letter word is derived
 		for (int w = 0; w < inputWords.length; w++) {
-			mapFourLetters(inputWords[w], fourLetterWords);
-			// if (fourLetterWords.contains(sortString(inputWords[w].substring(1)))) {
-			// 	// LinkedBag tempLb = fourLetterWords.get(sortString(inputWords[w].substring(1)));
-			// 	// tempLb.add(inputWords[w]);
-			// 	// fourLetterWords.put(sortString(inputWords[w].substring(1)), lb);
-			// 	StdOut.println(sortString(inputWords[w].substring(1)) + " already exists");
-				
-			// }
-			// else {
-			// 	//LinkedBag lb = new LinkedBag();
-			// 	mapFourLetters(inputWords[w], fourLetterWords);
-			// 	//lb.add(inputWords[w]);
-			// 	//fourLetterWords.put(sortString(inputWords[w].substring(1)), lb);
-			// }
+			mapFourLetters(inputWords[w], fourLetterWords, w);
+			indexFix.put(inputWords[w], w);
 		}
 
-		// Create an array with all "the last four" of all words in the file
-		// and sort it
-		for (int q = 0; q < inputWords.length; q++) {
-			inputWordsFour[q] = sortString(inputWords[q].substring(1));
-			//StdOut.println(inputWordsFour[q]);
-		}
-		Arrays.sort(inputWordsFour);
-
-
+		Digraph graph = new Digraph(inputWords.length);
 		
-		// int v = inputWords.length;
-		// Digraph graph = new Digraph(v);
-		// for(int i = 0; i < inputWords.length; i++){
-		// 	String s1 = inputWords[i];
-		// 	vertices.put(s1, i);
-		// 	for(int j = i+1; j < inputWords.length; j++){
-		// 		String s2 = inputWords[j];
-		// 		if(containLetters(s1, s2) && !s1.equals(s2)){graph.addEdge(i, j);}
-		// 		if(containLetters(s2, s1) && !s2.equals(s1)){graph.addEdge(j, i);}
-		// 	}
-		// }
-		// shortestPath("about","there", graph, vertices);
-		//StdOut.println(graph.toString());
-	}
+		for (int i = 0; i < inputWords.length; i++) {
+			if(fourLetterWords.contains(sortString(inputWords[i].substring(1)))) {
+				LinkedBag lb = (LinkedBag) fourLetterWords.get(sortString(inputWords[i].substring(1)));
 
+
+				for (Object o: lb) {
+					Vertex v = (Vertex) o;
+					if (!v.getWord().equals(inputWords[i])) {
+						graph.addEdge(i, v.getIndex());
+					}
+				}
+			}
+		}
+
+		shortestPath("zombi", "aargh", graph, indexFix);
+		shortestPath("aargh", "zombi", graph, indexFix);
+		shortestPath("altos", "duets", graph, indexFix);
+		shortestPath("daily", "hones", graph, indexFix);
+		shortestPath("bring", "beers", graph, indexFix);
+		shortestPath("oared", "oared", graph, indexFix);
+	}
 }
 
