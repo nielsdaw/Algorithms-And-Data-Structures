d1ba9823d0f1ff8ea2bb5c9992fff5fc8c7e38d9
jonastonny <jonastonnynielsen@gmail.com>
2015-04-23 15:41:07 +0200

    More commenting

--- Assignment 8/SpanningUSA.java
+++ Assignment 8/SpanningUSA.java
@@ -1,25 +1,25 @@
 public class SpanningUSA {
 
 	public static void main(String[] args) {
-		String[] input = StdIn.readAllLines(); // Read input. The format is 'String ' (with a space in the end as it is in USA-highway-miles.txt) and 'String--String [digits]'
-		ST verticies = new ST();
+		String[] input = StdIn.readAllLines(); // Read input. The format is 'String ' (with a space in the end as it is in USA-highway-miles.txt) and 'String--String [digits]'.
+		ST verticies = new ST(); // New symbol table.
 		
 		// Reads all verticies and adds them to a symbol table with Key: String, Value: i. Breaks when first edge is seen.
 		for (int i = 0; i < input.length; i++) {
-			if(input[i].contains("--")) {break;}
-			else {verticies.put(input[i], i);}
+			if(input[i].contains("--")) {break;} // Breaks if a string is containing '--', then it's an edge.
+			else {verticies.put(input[i], i);} // Adds (string, i) to verticies.
 		}
 		
 		EdgeWeightedGraph ewg = new EdgeWeightedGraph(verticies.size()); // Creates an EdgeWeightedGraph with size equal to number of verticies.
 		
 		// Connects the verticies with edges
 		for (int j = 0+ewg.V(); j < input.length; j++) {
-			String[] parts = input[j].split("--|\\s\\[|\\]"); // Regex which splits '"San Diego"--Utica [2802]' into '"San Diego"', 'Utica', '2802'.
+			String[] parts = input[j].split("--|\\s\\[|\\]"); // Regex which splits '"San Diego"--Utica [2802]' into '"San Diego"', 'Utica', '2802' (example).
 			Edge e = new Edge((Integer) verticies.get(parts[0]+" "), (Integer) verticies.get(parts[1]+" "), Integer.parseInt(parts[2])); // Create a new edge with above information.
-			ewg.addEdge(e); // Add edge to the EdgeWeightedGraph
+			ewg.addEdge(e); // Add edge to the EdgeWeightedGraph.
 		}
-		KruskalMST mst = new KruskalMST(ewg);
-		StdOut.println(mst.weight());
+		KruskalMST mst = new KruskalMST(ewg); // Create minimum spanning tree of EdgeWeightedGraph.
+		StdOut.println(mst.weight()); // Prints the weight of the minimum spanning tree.
 		StdOut.println(ewg.toString()); // Prints a representation of the graph. Comment/uncomment as necessary.
 	}
 }
