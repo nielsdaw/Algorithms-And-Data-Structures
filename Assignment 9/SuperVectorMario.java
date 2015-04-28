c47221fd79fdb6e570ee8cc091bbe09ab8f45f97
Niels Dawartz <nielsdaw@gmail.com>
2015-04-28 10:34:34 +0200

    added newest version

--- Assignment 9/SuperVectorMario.java
+++ Assignment 9/SuperVectorMario.java
@@ -1,16 +1,39 @@
 public class SuperVectorMario {
+	
 	public static void main(String[] args) {
+
 		int rows = StdIn.readInt();
 		int colums = StdIn.readInt();
-		String[] input = StdIn.readAllLines();
-		
+		StdIn.readChar(); 		// remove linespace
+
+		String[] input = StdIn.readAllLines();	// 
 		char[][] grid = new char[rows][colums];
 
-		for(int i = 1; i < rows; i++) {
+		Digraph graph = new Digraph(rows*colums);	// digraph
+
+		for(int i = 0; i < rows; i++) {
 			for(int j = 0; j < colums; j++) {
 				grid[i][j] = input[i].charAt(j);
+
 				StdOut.print(grid[i][j]);
+			}
+			StdOut.print("\n");
+		}
+	}
+
+
+	public void addToBoard(String s){
+
+		if(!s.equals("O")){
+			if(s.equals("S")){
+
+			}
+			else if(s.equals("F")){
+
+			}
+			else{
 			}
 		}
 	}
+
 }
